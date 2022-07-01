02826f92e2359a54fcb899ca8f25ca245afdb1eb
Suraj <surajsoni8840461898@gmail.com>
2022-07-01 21:13:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // colorChangeNav navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("colorChangeNav");
+            $('.home').addClass("changedHomeColor");
+        }else{
+            $('.navbar').removeClass("colorChangeNav");
+            $('.home').removeClass("changedHomeColor");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Technolgy and Science","Engineering","Management","Teachings"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["RRSIMTians","Engineers"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+
+function submitted(){
+ 
+    swal({
+        content: {
+            element: "p",
+            attributes: {
+                innerText: "Thanks for Contacting, You will get response soon",
+                style: "color: #1363DF;",
+                className: "p",
+            },
+        },
+    });
+}
